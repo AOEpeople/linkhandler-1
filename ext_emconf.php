261d5c6ddd59260bc481bf3c8984880c04974962
Christian Krzeminski <christian.krzeminski@aoe.com>
2019-08-09 11:21:28 +0200

    [TASK] Raise version to 4.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Generic record link handler',
     'description' => 'Create links to any record (e.g. news)',
     'category' => 'plugin',
-    'version' => '4.0.0',
+    'version' => '4.0.1',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -12,14 +12,14 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Francois Suter',
     'author_email' => 'typo3@cobweb.ch',
     'author_company' => 'Cobweb Development Sarl',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '7.6.1-7.9.99',
-        ),
-        'conflicts' => array(
+        ],
+        'conflicts' => [
             'ch_rterecords' => '',
             'tinymce_rte' => '',
-        ),
-        'suggests' => array(),
-    ),
+        ],
+        'suggests' => [],
+    ],
 );
